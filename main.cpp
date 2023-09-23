6031e026d8ce654c6b0598279419f09b119000d3
usefSaeed <youssef.okab@gmail.com>
2023-09-23 10:31:31 +0200

    starter code ready

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "helper.h"
+
+int main() {
+    vectorBasicTest();
+    vectorManipulation();
+    QueueStackBasicTest();
+    SetAndPairAndMapBasicTest();
+    stringBasicTest();
+    valarrayTest();
+    return 0;
+}
